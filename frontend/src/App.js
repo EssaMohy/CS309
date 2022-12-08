035d7624b672a7b514207b6d9798a446f635c668
EssaMohy <105460011+EssaMohy@users.noreply.github.com>
2022-12-08 15:50:01 +0200

    project

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <a href="/">amazona</a>
+        </header>
+        <main>list of products</main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
