2cf8428f4cbdfa6e07e2040697fae447b5706411
ahmed essam <120597294+AhmedEssamYouns@users.noreply.github.com>
2022-12-16 00:01:00 +0200

    update

--- fit-nutration/src/App.jsx
+++ fit-nutration/src/App.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-import HomePage from "./Pages/HomePage";
-const App = () => {
-  return <HomePage />;
-};
+
+import HomePage from './pages1/HomePage';
+import Login from './pages1/Login';
+import Products from './pages1/Products'
+
+function App() {
+  return (
+    <div className='App'><HomePage></HomePage></div>
+
+  );
+}
+
 export default App;
+
