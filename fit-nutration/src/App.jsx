8ac70b91174f507652d1b82477ba6078178fe091
EssaMohy <105460011+EssaMohy@users.noreply.github.com>
2022-12-16 00:22:02 +0200

    Main

--- fit-nutration/src/App.jsx
+++ fit-nutration/src/App.jsx
@@ -1,14 +1,11 @@
-
-import HomePage from './pages1/HomePage';
-import Login from './pages1/Login';
-import Products from './pages1/Products'
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className='App'><HomePage></HomePage></div>
-
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
   );
 }
 
 export default App;
-
