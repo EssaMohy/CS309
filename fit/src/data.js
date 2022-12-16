94c6918edf45debef1753d90fe5276cf6c39d6de
EssaMohy <105460011+EssaMohy@users.noreply.github.com>
2022-12-16 16:49:20 +0200

    Main

---
+++ fit/src/data.js
@@ -0,0 +1,51 @@
+export const categories = [
+  {
+    id: 1,
+    img: "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/313399689_655455652807747_373616896518390698_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=jE_l6fZEGyIAX8i1sjY&_nc_ht=scontent.fcai21-4.fna&oh=00_AfCoOeiJxOc3yXUeupUJkDZWamHW_IUQeSJE5PEmr1VoBg&oe=639D840A",
+    title: "ORGANIC NATION",
+  },
+  {
+    id: 2,
+    img: "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/301700060_474853714647970_4930241802244013257_n.png?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rxnfqKsX5D8AX-lz30t&_nc_ht=scontent.fcai21-4.fna&oh=00_AfDl7t6TDH0V4AZGEsieHlDDC8cABscO_4Do6hi818U0WA&oe=639D725B",
+    title: "MUSCLE ADD",
+  },
+  {
+    id: 3,
+    img: "https://scontent.fcai21-4.fna.fbcdn.net/v/t1.6435-9/46501508_1654430007996646_1850966102385885184_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=p_iBqnV3wnAAX9M_wyx&_nc_ht=scontent.fcai21-4.fna&oh=00_AfBI1jUHiIA8YegH-gPVYnFAdI94s2ddvBy744bn_9NumQ&oe=63C037D6",
+    title: "CHALLENGER NUTRITION",
+  },
+];
+export const popularProducts = [
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 2,
+    img: "",
+  },
+  {
+    id: 3,
+    img: "",
+  },
+  {
+    id: 4,
+    img: "",
+  },
+  {
+    id: 5,
+    img: "",
+  },
+  {
+    id: 6,
+    img: "",
+  },
+  {
+    id: 7,
+    img: "",
+  },
+  {
+    id: 8,
+    img: "",
+  },
+];
