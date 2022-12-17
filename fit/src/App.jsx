371957f18da0315d5cb82f2accd151f3b5cf5339
ahmed essam <ahmedessamyounsa@example.com>
2022-12-18 01:22:06 +0200

    update

--- fit/src/App.jsx
+++ fit/src/App.jsx
@@ -1,6 +1,37 @@
 import React from "react";
 import HomePage from "./Pages/HomePage";
+import React from 'react';
+import HomePage from './Pages1/Homepage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Products from './Products/Products';
+import ProductsCHAll from './Products/ProductsCHAll'
+import ProductsMuscleADD from './Products/ProductsMuscleADD';
+import ProductsORGA from './Products/ProductsORGA';
+import Login from './Pages1/Login';
+import Signup from './Pages1/Signup'
+
 const App = () => {
-  return <HomePage />;
-};
+  // return <HomePage />;
+ 
+    return (
+      <div className='App'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/Products' element={<Products />} />
+            <Route path='/Products1' element={<ProductsCHAll/>} />
+            <Route path='/Products2' element={<ProductsMuscleADD/>} />
+            <Route path='/Products3' element={<ProductsORGA/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signup' element={<Signup/>} />
+          </Routes>
+        </Router>
+  
+  
+  
+      </div>
+    );
+  }
+  
+
 export default App;
