5a2a564423598a09a2ffba58e9f88ceba4d91764
SaherMohamed250 <120128502+SaherMohamed250@users.noreply.github.com>
2022-12-20 01:37:12 +0200

    Update Login.js

--- fit/src/Pages/Login.js
+++ fit/src/Pages/Login.js
@@ -5,19 +5,18 @@ import { Link } from 'react-router-dom';
 function Login() {
     return (<Fragment>
 <header><Navbar></Navbar></header>
-<div className='cover'>
+       <div className='cover'>
     <form>
-      <h1 className='text1'>Login</h1>
+      <h1 className='text1'>SignIn</h1>
             <input type="text" placeholder="username" required/>
             <input type="password" placeholder="password" required />
             <div className='su'>
             <button type='submit'>Login</button>
             </div>
             <div className='newacc'>
-            <p className='don'>don't have an account ? <Link to='/signup' className='sighnup'>Sign Up</Link></p>
+            <p className='don'>don't have an account ?  <Link to='/signup'className='sighnup'>Sign Up</Link></p>
             </div>
             </form>
-    </div>
     </div>
 
 
