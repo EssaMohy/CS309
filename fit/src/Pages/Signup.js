c9c665a9d64e4f56d1f0ec75bee90156470ac82e
SaherMohamed250 <120128502+SaherMohamed250@users.noreply.github.com>
2022-12-20 01:25:15 +0200

    Update Signup.js

--- fit/src/Pages/Signup.js
+++ fit/src/Pages/Signup.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Signup() {
     return (<Fragment>
 <header><Navbar></Navbar></header>
-      <div className='cover2'>
+       <div className='cover2'>
 <form>
   <h1 className='text1'>register</h1>
   <input type="text" placeholder="First Name" required/>
@@ -13,11 +13,11 @@ function Signup() {
   <input type="email" placeholder="E-mail address" required/>
   <input type="password" placeholder="Enter Password" required/>
   <input type="password" placeholder="Confirm password" required/>
+  <div className='newacc'>
+        <p className='don'>already have an account <Link to='/signup'>Sign In</Link></p>
         <div className='su'>
         <button type='submit'>Sign Up</button>
         </div>
-        <div className='newacc'>
-        <p className='don'>already have an account <Link to="/login" className='sighnup'>login</Link></p>
         </div>
         </form>
 </div>
