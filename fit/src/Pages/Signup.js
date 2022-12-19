4dca9ae145351790622975b37cbb48d16e890895
SaherMohamed250 <120128502+SaherMohamed250@users.noreply.github.com>
2022-12-20 01:06:57 +0200

    Update Signup.js

--- fit/src/Pages/Signup.js
+++ fit/src/Pages/Signup.js
@@ -5,27 +5,22 @@ import { Link } from 'react-router-dom';
 function Signup() {
     return (<Fragment>
 <header><Navbar></Navbar></header>
-<div class="signup-box">
-      <h1>Sign Up</h1>
-      <h4>It's free and only takes a minute</h4>
-      <form>
-        <label>First Name</label>
-        <input type="text" placeholder="" />
-        <label>Last Name</label>
-        <input type="text" placeholder="" />
-        <label>Email</label>
-        <input type="email" placeholder="" />
-        <label>Password</label>
-        <input type="password" placeholder="" />
-        <label>Confirm Password</label>
-        <input type="password" placeholder="" />
-        <input type="button" class='btn' value="Submit" />
-      <closeform></closeform></form>
-
-    </div>
-    <p class="para-2">
-      Already have an account? <Link to="/login">login</Link>
-    </p>
+      <div className='cover2'>
+<form>
+  <h1 className='text1'>register</h1>
+  <input type="text" placeholder="First Name" required/>
+  <input type="text" placeholder="Last Name" required/>
+  <input type="email" placeholder="E-mail address" required/>
+  <input type="password" placeholder="Enter Password" required/>
+  <input type="password" placeholder="Confirm password" required/>
+        <div className='su'>
+        <button type='submit'>Sign Up</button>
+        </div>
+        <div className='newacc'>
+        <p className='don'>already have an account <Link to="/login" className='sighnup'>login</Link></p>
+        </div>
+        </form>
+</div>
 
 
     </Fragment>)
