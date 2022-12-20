d6e6778ece19f31b8d846a696fc8babac890d453
ahmed essam <ahmedessamyounsa@example.com>
2022-12-20 12:08:34 +0200

    update

--- fit/src/Pages/Navbar.js
+++ fit/src/Pages/Navbar.js
@@ -76,8 +76,6 @@ function Navbar() {
             </li>
           </ul>
         </nav>
-        <div class="header-actions" />
-
         <div class="search-container">
     <form action="">
       <input type="text" placeholder="Search..." name="search"/>
