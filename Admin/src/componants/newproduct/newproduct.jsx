91dde3dfce7551989019f707a542ca12bde3c89a
SaherMohamed250 <120128502+SaherMohamed250@users.noreply.github.com>
2022-12-25 05:18:29 +0200

    Update newproduct.jsx

--- Admin/src/componants/newproduct/newproduct.jsx
+++ Admin/src/componants/newproduct/newproduct.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import "./newproduct.css"
 function newproduct() {
   return (
-    <div className="newProduct">
-      <h1 className="ProductTitle">Add Products</h1>
       <form className="ProductForm">
+      <h1 className="ProductTitle">Add Products</h1>
         <div className="ProductItem">
           <label>Image</label>
           <input type="file" id="file" />
@@ -18,48 +17,14 @@ function newproduct() {
           <input type="text" placeholder="Product Price" required/>
         </div>
         <div className="ProductItem">
-          <label>Stock</label>
-          <input type="text" placeholder="122" />
-        </div>
-        <div className="ProductItem">
           <label>discribtion</label>
           <input type="text" className='dis' placeholder="it's a good item" />
         </div>
         <button className="ProductButton">Create</button>
       </form>
-    </div>
+    
   );
 }
 
 export default newproduct;
 
-
-
-
-
-// {/* <div className="newProduct">
-//       <h1 className="ProductTitle">New Product</h1>
-//       <form className="ProductForm">
-//         <div className="ProductItem">
-//           <label>Image</label>
-//           <input type="file" id="file" />
-//         </div>
-//         <div className="ProductItem">
-//           <label>Name</label>
-//           <input type="text" placeholder="Product Name" required/>
-//         </div>
-//         <div className="ProductItem">
-//           <label>Price</label>
-//           <input type="text" placeholder="Product Price" required/>
-//         </div>
-//         <div className="ProductItem">
-//           <label>Stock</label>
-//           <input type="text" placeholder="122" />
-//         </div>
-//         <div className="ProductItem">
-//           <label>discribtion</label>
-//           <input type="text" className='dis' placeholder="it's a good item" />
-//         </div>
-//         <button className="ProductButton">Create</button>
-//       </form>
-//     </div> */}
