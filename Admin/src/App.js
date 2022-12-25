effb312f7d1e6aca9227f144feddf9bb1ed40700
EssaMohy <105460011+EssaMohy@users.noreply.github.com>
2022-12-26 00:52:36 +0200

    main

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./App.css";
 import Topbar from "./componants/topbar/topbar";
-import Newm from "./componants/newM/newM";
-
-import Product from "./componants/product/product";
 
 function App() {
   return (
