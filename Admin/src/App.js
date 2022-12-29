252ac9163b29e5a93a49dc6bdff2bd971ae79042
EssaMohy <105460011+EssaMohy@users.noreply.github.com>
2022-12-29 11:48:56 +0200

    main

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topbar from "./componants/topbar/topbar";
-import Newproduct from "./componants/newproduct/newproduct"
-import Dproduct from "./componants/Dproduct/Dproduct"
+import Newproduct from "./componants/newproduct/newproduct";
+import Dproduct from "./componants/Dproduct/Dproduct";
 import Product from "./componants/Uproduct/Uproduct";
-import Pro from "./pages/proList";
-import Hhome from "./pages/Home";
+import Pro from "./Pages/proList";
+import Hhome from "./Pages/Home";
 
 const App = () => {
   return (
@@ -14,9 +14,9 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Hhome></Hhome>} />
-          <Route path="/pro" element={<Pro/>} />
-          <Route path="/new" element={<Newproduct/>} />
-          <Route path="/edit" element={<Product/>} />
+          <Route path="/pro" element={<Pro />} />
+          <Route path="/new" element={<Newproduct />} />
+          <Route path="/edit" element={<Product />} />
         </Routes>
       </Router>
     </div>
